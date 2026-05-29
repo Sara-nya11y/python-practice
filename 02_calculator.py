3245f4e7a54eced498778dbf1f8571585bb8576d
Sara-nya11y <veerankisaranya656@gmail.com>
2026-05-29 21:15:52 +0530

    Create 02_calculator.py

---
+++ 02_calculator.py
@@ -0,0 +1,34 @@
+# Program: Simple Calculator
+# Concept: Input, Variables, If-Else, Operators
+# Author: Sara-nya11y
+# Description: User nunchi 2 numbers teeskuni +, -, *, / operation chestadi.
+
+# User nunchi modati number teeskuntunnam
+num1 = float(input("Modati number ivvu: "))
+
+# Rendo number teeskuntunnam  
+num2 = float(input("Rendo number ivvu: "))
+
+# Operation select cheskovadam
+print("Operation select chey: +, -, *, /")
+operator = input("Operator ivvu: ")
+
+# If-else tho calculation chestunnam
+if operator == '+':
+    result = num1 + num2
+    print("Result:", result)
+elif operator == '-':
+    result = num1 - num2
+    print("Result:", result)
+elif operator == '*':
+    result = num1 * num2
+    print("Result:", result)
+elif operator == '/':
+    # Zero tho divide cheyakunda check
+    if num2 == 0:
+        print("Error: Zero tho divide cheyodhu!")
+    else:
+        result = num1 / num2
+        print("Result:", result)
+else:
+    print("Invalid operator! +, -, *, / lo okati ivvu")

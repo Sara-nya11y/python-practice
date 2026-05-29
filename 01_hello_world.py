d9bb0f6ec0e667b19194e1a01b64340e64769ce9
Sara-nya11y <veerankisaranya656@gmail.com>
2026-05-29 21:06:17 +0530

    Update and rename README.md to 01_hello_world.py

---
+++ 01_hello_world.py
@@ -0,0 +1,6 @@
+# Program: Hello World
+# Concept: Basic Print Statement
+# Author: Sara-nya11y
+# Description: Python lo modati program. Screen meeda text print cheyadam nerchukuntam.
+
+print("Hello, World!")  # Output ni console lo chupistundi

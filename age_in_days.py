d7cb6aee05d61baab9e013eacc47635cb24bcbf7
Sara-nya11y <veerankisaranya656@gmail.com>
2026-06-09 18:39:02 +0530

    Create age_in_days.py

---
+++ age_in_days.py
@@ -0,0 +1,9 @@
+print("--- Age in Days Calculator ---")
+
+try:
+    age = int(input("Nee age years lo cheppu: "))
+    days = age * 365
+    print(f"Nuvvu putti roughly {days} days aindhi 😄")
+    print(f"Exact ga cheppalante leap years kuda lekapovali")
+except ValueError:
+    print("Number ivvu chitti")

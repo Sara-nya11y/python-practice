860beaf648b08c8ef82049110afe8beca3ab5439
Sara-nya11y <veerankisaranya656@gmail.com>
2026-06-09 18:35:55 +0530

    Create dice_roller.py

---
+++ dice_roller.py
@@ -0,0 +1,10 @@
+import random
+
+print("--- Dice Roller ---")
+input("Enter nokki dice throw chey: ")
+
+dice = random.randint(1, 6)
+print(f"Nuvvu kottindhi: {dice} 🎲")
+
+if dice == 6:
+    print("Super! 6 vachindhi 🔥")

dbdb97d855b787c4df80680b9a910f12a6f1a5b7
Sara-nya11y <veerankisaranya656@gmail.com>
2026-06-10 19:00:01 +0530

    Update armstrong_number.py

--- armstrong_number.py
+++ armstrong_number.py
@@ -1,8 +1,15 @@
-n = int(input())
-temp = n
-sum = 0
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
+# Program: Armstrong Number Checker
+# Concept: While loop, Modulus, Exponents
+# Author: Sara-nya11y
+# Logic: 153 = 1^3 + 5^3 + 3^3 = 153
+
+n = int(input("Number ivvu: ")) 
+temp = n 
+sum = 0  
+while temp > 0: 
+    digit = temp % 10    
+    sum += digit ** 3  
+    temp //= 10          
+
+
 print("Armstrong" if sum == n else "Not Armstrong")

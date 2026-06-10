a280dd27544769ad9608b5b00452f7d26bc47cb1
Sara-nya11y <veerankisaranya656@gmail.com>
2026-06-10 18:56:10 +0530

    Update 01_hello_world.py

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,6 +1,2 @@
-# Program: Hello World
-# Concept: Basic Print Statement
-# Author: Sara-nya11y
-# Description: Python lo modati program. Screen meeda text print cheyadam nerchukuntam.
 
 print("Hello, World!")  # Output ni console lo chupistundi
